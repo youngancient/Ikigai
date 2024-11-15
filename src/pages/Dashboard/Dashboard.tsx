31a8d874c6ab72b9cf992b6db9ca2a5ce52faf0b
Collins Adi <admin@admins-MacBook-Air.local>
2024-11-15 16:59:43 +0100

    Fixed Merge Conflict

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,9 @@
+import { Layout } from "../../components/layout/Layout";
+
+export const Dashboard = () => {
+  return (
+    <Layout title="Dashboard">
+      <h3>Hello Party People</h3>
+    </Layout>
+  );
+};
