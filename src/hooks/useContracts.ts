17b8c8b350ae377786a0eca1ddb0a5469fa1c15d
youngancient <tochyokoye@gmail.com>
2024-11-18 21:06:01 +0100

    added contracts and structure for contract hook usage

--- src/hooks/useContracts.ts
+++ src/hooks/useContracts.ts
@@ -1,45 +1,45 @@
 import { useMemo } from "react";
 import useRunners from "./useRunners";
 import { Contract } from "ethers";
-import FACTORY_ABI from "../ABI/factory.json";
-import AIRDROP_ABI from "../ABI/airdrop.json";
+import TRUSTFUND_ABI from "../ABI/trustfund.json";
+import WILL_ABI from "../ABI/will.json";
 
 
-export const useFactoryContract = (withSigner = false) => {
+export const useTrustFundContract = (withSigner = false) => {
     const { readOnlyProvider, signer } = useRunners();
 
     return useMemo(() => {
         if (withSigner) {
             if (!signer) return null;
             return new Contract(
-                import.meta.env.VITE_FACTORY_CONTRACT_ADDRESS,
-                FACTORY_ABI,
+                import.meta.env.VITE_TRUSTFUND_CONTRACT_ADDRESS,
+                TRUSTFUND_ABI,
                 signer
             );
         }
         return new Contract(
-            import.meta.env.VITE_FACTORY_CONTRACT_ADDRESS,
-            FACTORY_ABI,
+            import.meta.env.VITE_TRUSTFUND_CONTRACT_ADDRESS,
+            TRUSTFUND_ABI,
             readOnlyProvider
         );
     }, [readOnlyProvider, signer, withSigner]);
 };
 
-export const useAidropContract = (withSigner = false, airdropContractAddress : string) => {
+export const useWillContract = (withSigner = false) => {
     const { readOnlyProvider, signer } = useRunners();
 
     return useMemo(() => {
         if (withSigner) {
             if (!signer) return null;
             return new Contract(
-                airdropContractAddress,
-                AIRDROP_ABI,
+                import.meta.env.VITE_WILL_CONTRACT_ADDRESS,
+                WILL_ABI,
                 signer
             );
         }
         return new Contract(
-            airdropContractAddress,
-            AIRDROP_ABI,
+            import.meta.env.VITE_WILL_CONTRACT_ADDRESS,
+            WILL_ABI,
             readOnlyProvider
         );
     }, [readOnlyProvider, signer, withSigner]);
