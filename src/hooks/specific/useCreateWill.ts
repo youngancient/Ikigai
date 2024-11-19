3867371285d76fe7892f2905db7c96f5e2efb8cd
youngancient <tochyokoye@gmail.com>
2024-11-19 22:16:25 +0100

    omo

--- src/hooks/specific/useCreateWill.ts
+++ src/hooks/specific/useCreateWill.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { useWillContract } from "../useContracts";
 import { useAppKitAccount, useAppKitNetwork } from "@reown/appkit/react";
 import { liskSepoliaNetwork } from "../../connection";
@@ -91,49 +91,4 @@ export const useRegisterWill = (tokenAddress: string) => {
   return { registerWill, isRegisterLoading, isDone };
 };
 
-interface IWill {
-  id: BigInt;
-  owner: string;
-  name: string;
-  lastActivity: BigInt;
-  isActive: boolean;
-  etherAllocation: BigInt;
-  gracePeriod: BigInt;
-  activityThreshold: BigInt;
-  deadManSwitchTriggered: boolean;
-  deadManSwitchTimestamp: BigInt;
-}
-export const useWill = () => {
-  const [will, setWill] = useState<IWill | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
 
-  const { address } = useAppKitAccount();
-  const readOnlyWillRegistry = useWillContract();
-  const fetchWill = useCallback(async () => {
-    if (!readOnlyWillRegistry) {
-      setWill(null);
-      return;
-    }
-    if (!address) {
-      setWill(null);
-      return;
-    }
-    try {
-      setIsLoading(true);
-      const _will = await readOnlyWillRegistry.wills(address);
-      console.log(_will);
-      setWill(_will);
-    } catch (error) {
-      setWill(null);
-      console.log(error);
-    } finally {
-      setIsLoading(false);
-    }
-  }, [readOnlyWillRegistry, address]);
-
-  useEffect(() => {
-    fetchWill();
-  }, [fetchWill]);
-
-  return { will, isLoading };
-};
