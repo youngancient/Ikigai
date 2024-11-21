0bec724feda7cf84eb416edee43585ed0ecaeb61
youngancient <tochyokoye@gmail.com>
2024-11-21 10:19:54 +0100

    added landing page animations and bug-fixes

--- src/animations/landing.ts
+++ src/animations/landing.ts
@@ -27,3 +27,54 @@ export const textVariant = {
       },
     },
   };
+
+  export const leftVariant = {
+    initial: {
+      opacity: 0,
+      x: -100,
+    },
+    final: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+    final2: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: 0.125,
+        duration: 0.5,
+      },
+    },
+    final3: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: 0.25,
+        duration: 0.5,
+      },
+    },
+  };
+
+  export const contVariant = {
+    initial: {
+      opacity: 0,
+      y: 80,
+    },
+    final: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.6,
+      },
+    },
+    exit: {
+      opacity: 1,
+      y: -20,
+      transition: {
+        duration: 0.25,
+      },
+    },
+  };
