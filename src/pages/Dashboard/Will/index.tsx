8fcd4307ba4654d33f6f067e441ef2a471553e0b
Raji Mustapha <rajimustapha30@gmail.com>
2024-11-25 17:31:38 +0100

    chore: list wills

--- src/pages/Dashboard/Will/index.tsx
+++ src/pages/Dashboard/Will/index.tsx
@@ -134,14 +134,20 @@ const WillPage = () => {
               <div className="total-balance">
                 <p>Total Balance</p>
                 <h3>
-                  {isLoadingBalance
-                    ? "Loading..."
-                    : tokenBalance != null
-                    ? floorToDecimals(
-                        parseFloat(ethers.formatUnits(tokenBalance, 18)),
-                        2
-                      )
-                    : 0.0}{" "}
+                  {isLoadingBalance ? (
+                    <div role="status" className="max-w-sm animate-pulse">
+                      <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                    </div>
+                  ) : tokenBalance != null ? (
+                    floorToDecimals(
+                      parseFloat(ethers.formatUnits(tokenBalance, 18)),
+                      2
+                    )
+                  ) : (
+                    0.0
+                  )}{" "}
                   {isLoadingBalance ? "" : userSelectedToken?.symbol}
                 </h3>
               </div>
@@ -196,7 +202,28 @@ const WillPage = () => {
               </div>
 
               <div className="vault-list">
-                {wills !== null && wills?.length ? (
+                {isFetching && (
+                  <div
+                    role="status"
+                    className="w-full mt-4 space-y-4  divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 dark:border-gray-700"
+                  >
+                    <div className="flex items-center justify-between">
+                      <div>
+                        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+                        <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                      </div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                    </div>
+                    <div className="flex items-center justify-between pt-4">
+                      <div>
+                        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+                        <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                      </div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                    </div>
+                  </div>
+                )}
+                {wills !== null && !isFetching && wills?.length ? (
                   wills?.map((item, i) => {
                     if (i < 2) {
                       return (
@@ -259,8 +286,31 @@ const WillPage = () => {
             <div className={`activity-table `}>
               {address ? (
                 isFetching ? (
-                  <div className="flex justify-center items-center h-32 w-full">
-                    <CircularProgress size="2rem" sx={{ color: "#ffffff" }} />
+                  <div className="flex justify-start items-center gap-8 flex-wrap ">
+                    {[1, 2, 3].map((item) => (
+                      <div
+                        key={item}
+                        role="status"
+                        className=" w-full  rounded shadow animate-pulse md:w-[300px] dark:border-gray-700"
+                      >
+                        <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
+                          <svg
+                            className="w-10 h-10 text-gray-200 dark:text-gray-600"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 16 20"
+                          >
+                            <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                            <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+                          </svg>
+                        </div>
+                        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                      </div>
+                    ))}
                   </div>
                 ) : (
                   <div className="will-list-flex">
